8611c59e5ea23ab3292d9c7e7c2a61c61a521d9b
Advik Kant <142683161+EpicNerdGuy@users.noreply.github.com>
2025-06-25 19:55:14 +0530

    Updated client.c

--- client.c
+++ client.c
@@ -6,52 +6,59 @@
 #include <unistd.h>
 #include <string.h>
 #include <pthread.h>
+
 #define PORT 8086
-void *send_handler(void *arg){
-	int c=*((int *) arg);
-	char message[1024];
-	while(1){
-		fgets(message,sizeof(message),stdin);
-		send(c,message,sizeof(message),0);
-	}
+
+void *send_handler(void *arg) {
+    int c = *((int *)arg);
+    char message[1024];
+
+    while (1) {
+        if (fgets(message, sizeof(message), stdin) != NULL) {
+            send(c, message, strlen(message), 0);
+        }
+    }
+
+    return NULL;
 }
 
-void *recv_handler(void *arg){
-	int c=*((int *) arg);
-	char buffer[1024];
-	int length;
-	while((length=recv(c,buffer,sizeof(buffer)-1,0))>0){
-		buffer[length]='\0';
-		printf("OTHER USER-%s",buffer);
-	}
-	printf("DISCONNECTED FROM SERVER\n");
-	exit(0);
+void *recv_handler(void *arg) {
+    int c = *((int *)arg);
+    char buffer[1024];
+    int length;
+
+    while ((length = recv(c, buffer, sizeof(buffer) - 1, 0)) > 0) {
+        buffer[length] = '\0';
+        printf("OTHER USER: %s", buffer);
+    }
+
+    printf("DISCONNECTED FROM SERVER\n");
+    exit(0);
 }
 
+int main() {
+    struct sockaddr_in caddr;
+    int c = socket(AF_INET, SOCK_STREAM, 0);
 
+    caddr.sin_family = AF_INET;
+    caddr.sin_port = htons(PORT);
+    caddr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
+    if (connect(c, (struct sockaddr*)&caddr, sizeof(caddr)) != 0) {
+        perror("FAILED TO CONNECT TO SERVER");
+        close(c);
+        exit(EXIT_FAILURE);
+    }
 
+    printf("Connected to the server.\n");
 
-int main(){
-	struct sockaddr_in caddr;
-	int c=socket(AF_INET,SOCK_STREAM,0);
-	caddr.sin_family=AF_INET;
-	caddr.sin_port=htons(PORT);
-	caddr.sin_addr.s_addr=inet_addr("127.0.0.1");
+    pthread_t sid, recvid;
+    pthread_create(&sid, NULL, send_handler, &c);
+    pthread_create(&recvid, NULL, recv_handler, &c);
 
-	if(connect(c,(struct sockaddr*)&caddr,sizeof(caddr))!=0){
-		perror("FAILED TO CONNECT TO SERVER");
-		close(c);
-		exit(EXIT_FAILURE);
-	}
+    pthread_join(sid, NULL);
+    pthread_join(recvid, NULL);
 
-	pthread_t sid,recvid;
-	pthread_create(&sid,NULL,send_handler,&c);
-	pthread_create(&recvid,NULL,recv_handler,&c);
-	pthread_join(sid,NULL);
-	pthread_join(recvid,NULL);
-	printf("BOTH THREADS ARE FINISHED WORKING\n");
-	return 0;
-
-
+    printf("BOTH THREADS HAVE FINISHED WORKING\n");
+    return 0;
 }
